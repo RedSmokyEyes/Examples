3fe6105115852abb139bf39636baa65a065fffa8
RedSmokyEyes <eazy911@yandex.ru>
2023-01-10 23:49:14 +0300

    add method array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {12, 20, 35, 4, 59, 66, 78, 82,};
+
+int n = array.Length;
+int find = 82;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index + 1;
+    index++;
+}
