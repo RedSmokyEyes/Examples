d10085dc0b134f6a25302d4d0ce1442c8ad50b5c
RedSmokyEyes <eazy911@yandex.ru>
2023-01-11 00:45:59 +0300

    add array library2

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -2,7 +2,7 @@
 {
     int length = collection.Length;
     int index = 0;
-    while (index < length)
+        while (index < length)
     {
         collection[index] = new Random().Next(1, 10);
         //index = index +1
@@ -21,7 +21,27 @@ void PrintArray(int[] collection)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index ++;
+    }
+    return position;
+}
+
 int[] array = new int [10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
